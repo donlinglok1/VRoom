8ec2cef4a6642ecbd3de5f6e559b97a670e92e0a
Kenneth Tu <donlinglok@gmail.com>
2016-04-06 15:05:48 +0800

    Update iOS_UIWebView.swift

--- iOS_UIWebView.swift
+++ iOS_UIWebView.swift
@@ -9,7 +9,17 @@ class VRoomControl: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
-        var url = NSURL(string:"https://darkdusk.org/vroom/api/viewer.php?key=$YOUR_API_KEY&img=4.jpg");
+	    //basic mode with server demo image
+        var url = NSURL(string:"https://darkdusk.org/vroom/api/viewer.php?key=/*YOUR API KEY*/&img=/*YOUR IMG PATH*/");
+        self.webView!.loadRequest(NSURLRequest(URL:url!));
+        
+	    //AR mode, please use mobile device testing
+        var url = NSURL(string:"https://darkdusk.org/vroom/api/viewer.php?key=/*YOUR API KEY*/&img=/*YOUR IMG PATH*/&mo=AR");
+        self.webView!.loadRequest(NSURLRequest(URL:url!));
+        
+	    //VR mode, please use mobile device testing
+	    //You can use full path to load image, but make sure it fit "Cross-Origin Resource Sharing policy"
+        var url = NSURL(string:"https://darkdusk.org/vroom/api/viewer.php?key=/*YOUR API KEY*/&img=/*YOUR IMG PATH*/&mo=VR");
         self.webView!.loadRequest(NSURLRequest(URL:url!));
     }
 }
