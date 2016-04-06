4fdda28985963364e6d9a4f82bb624b1507c5b89
Kenneth Tu <donlinglok@gmail.com>
2016-04-06 15:03:23 +0800

    Update PHP_cURL.php

--- PHP_cURL.php
+++ PHP_cURL.php
@@ -1,17 +1,17 @@
 <?php
 	//basic mode with server demo image
-	$curl_init = curl_init("https://darkdusk.org/vroom/api/viewer.php?key=$YOUR_API_KEY&img=4.jpg");
+	$curl_init = curl_init("https://darkdusk.org/vroom/api/viewer.php?key=$YOUR_API_KEY&img=$YOUR_IMG_PATH");
 	curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
 	echo curl_exec($curl_init)//html
-	
+
 	//AR mode, please use mobile device testing
-	$curl_init = curl_init("https://darkdusk.org/vroom/api/viewer.php?key=$YOUR_API_KEY&mo=AR&img=4.jpg");
+	$curl_init = curl_init("https://darkdusk.org/vroom/api/viewer.php?key=$YOUR_API_KEY&img=$YOUR_IMG_PATH&mo=AR");
 	curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
 	echo curl_exec($curl_init)//html
 	
 	//VR mode, please use mobile device testing
 	//You can use full path to load image, but make sure it fit "Cross-Origin Resource Sharing policy"
-	$curl_init = curl_init("https://darkdusk.org/vroom/api/viewer.php?key=$YOUR_API_KEY&mo=VR&img=https://community.htc.com/tw/data/attachment/forum/201411/24/190922byyqrytlmrwufqky.jpg");
+	$curl_init = curl_init("https://darkdusk.org/vroom/api/viewer.php?key=$YOUR_API_KEY&img=$YOUR_IMG_PATH&mo=VR");
 	curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
 	echo curl_exec($curl_init)//html
 ?>
